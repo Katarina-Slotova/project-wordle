999693eff0e94899fc3e624d08475f785992091a
Katarina-Slotova <katarina.slotova@gmail.com>
2023-11-20 18:03:49 -0500

    Add new component Guess for each individal guessed word.

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Guess from '../Guess'
 
 function PreviousGuesses({ previousWords }) {
   return (
     <div className='guess-results'>
-      {previousWords.map(({value, id}) => (
-        <p key={id} className='guess'>{value}</p>
+      {previousWords.map(({ value, id }) => (
+        <p key={id} className='guess'>
+          <Guess value={value} />
+        </p>
       ))}
     </div>
   )
