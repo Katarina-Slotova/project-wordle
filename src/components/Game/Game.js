b707b408cc6df32d3accfd0b895c8777b6a86021
Katarina-Slotova <katarina.slotova@gmail.com>
2023-11-21 16:04:17 -0500

    Modify Game component to check for correct answer.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils'
 import { WORDS } from '../../data'
 import GuessWord from '../GuessWord'
 import PreviousGuesses from '../PreviousGuesses'
+import GameOver from '../GameOver/GameOver'
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS)
@@ -12,15 +13,29 @@ console.info({ answer })
 
 function Game() {
   const [previousWords, setPreviousWords] = React.useState([])
+  const [gameStatus, setGameStatus] = React.useState('')
 
   function handleGuessList(guess) {
     setPreviousWords([...previousWords, guess])
+    if (answer === guess) {
+      setGameStatus('win')
+    } else if (previousWords.length >= 5 && answer !== guess) {
+      setGameStatus('lose')
+    }
   }
 
   return (
     <>
       <PreviousGuesses previousWords={previousWords} answer={answer} />
-      <GuessWord handleGuessList={handleGuessList} />
+      {gameStatus !== '' ? (
+        <GameOver
+          gameStatus={gameStatus}
+          answer={answer}
+          numOfGuesses={previousWords.length}
+        />
+      ) : (
+        <GuessWord handleGuessList={handleGuessList} />
+      )}
     </>
   )
 }
