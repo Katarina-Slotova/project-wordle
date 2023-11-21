dfefc5e68152d7eed3508fc182083c6d0cac0d60
Katarina-Slotova <katarina.slotova@gmail.com>
2023-11-21 09:58:07 -0500

    Modify PreviousGUesses component to allow generation of guess fields.

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Guess from '../Guess'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+import { range } from '../../utils'
 
 function PreviousGuesses({ previousWords }) {
   return (
     <div className='guess-results'>
-      {previousWords.map(({ value, id }) => (
-        <p key={id} className='guess'>
-          <Guess value={value} />
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={previousWords[num]} />
       ))}
     </div>
   )
