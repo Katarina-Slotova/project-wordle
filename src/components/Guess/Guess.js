4607b7967c866a5a8fbbf3bd2d8bfbe83abff6c3
Katarina-Slotova <katarina.slotova@gmail.com>
2023-11-21 14:33:34 -0500

    Add correct/misplaced/incorrect color indicators to letters in guessed words.

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ value }) {
-  console.log(value)
+function Cell({ letter, status }) {
+  return <span className={`cell ${status}`}>{letter}</span>
+}
+
+function Guess({ value, answer }) {
+  const updatedTentativeGuess = checkGuess(value, answer)
+
   return (
     <>
       <p className='guess'>
         {range(5).map((num) => (
-          <span key={num} className='cell'>
-            {value ? value[num] : undefined}
-          </span>
+          <Cell
+            key={num}
+            letter={
+              updatedTentativeGuess
+                ? updatedTentativeGuess[num].letter
+                : undefined
+            }
+            status={
+              updatedTentativeGuess ? updatedTentativeGuess[num].status : ''
+            }
+          />
         ))}
       </p>
     </>
